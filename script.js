c6ee54e94a8da2bf44f11a9a2522fdef1b73a6e1
Gustavo Raeski <gustavoraeski@outlook.com>
2020-06-12 20:43:02 -0300

    Adicionando o evento de fechar o modal

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let modalQt = 1;
 const c = (el) => document.querySelector(el);
 const cs = (el)=> document.querySelectorAll(el);
 
+// Listagem das pizzas
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     
@@ -41,4 +42,15 @@ pizzaJson.map((item, index)=>{
 
 
     c('.pizza-area').append(pizzaItem);
+});
+
+// Eventos do MODAL
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) =>{
+    item.addEventListener('click', closeModal);
 });
